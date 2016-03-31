3eeee56a8f89b3c54b0dc24c55f1733d552c6ef5
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2016-03-31 22:38:32 +0200

    Talk to Spotify's api (without auth token yet). Check if data about one of the songs are available and what they contain

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var request = require("request");
 var cheerio = require("cheerio");
+var SpotifyWebApi = require("spotify-web-api-node");
+var spotifyApi = new SpotifyWebApi();
+
+// TODO create public playlist
 
 // download lp3.pl's list of top songs of all time
 
 var url = "http://www.lp3.pl/alpt.phtml?m=29";
+
+var count = 0;
 
 request(url, function(error, response, html) {
 	if (!error) {
@@ -17,6 +23,17 @@ request(url, function(error, response, html) {
 				title: title
 			};
 			songs.push(song);
+			
+			count++;
+			if (count === 5) {
+				spotifyApi.searchTracks("track:" + title + " artist:" + singer)
+					.then(function(data) {
+						console.log(data.body.tracks.items);
+					}, function(err) {
+						console.log("Error downloading data");
+						console.log(err);
+					});
+			}
 		});
 		console.log('Found ' + songs.length + ' songs');
 	}
